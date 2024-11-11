16f8eed41a62cb08bef63310705b2688260b4472
yongin <rladls6412@naver.com>
2024-11-11 16:18:58 +0900

    추가버튼 누르면 로컬 스토리지에 추가하는 기능 완료

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,9 +1,31 @@
 const bookmarkInputForm = document.getElementById("bookmark_input_form");
 
+let bookmarkList = [];
+if (localStorage.getItem("bookmarkList")) {
+  bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
+} else {
+  localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
+}
+
+const addBookmarkItem = () => {
+  let bookmarkList = [];
+  if (localStorage.getItem("bookmarkList")) {
+    bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
+  }
+  let name = document.getElementById("name_input").value;
+  let url = document.getElementById("url_input").value;
+  let createAt = Date.now();
+  bookmarkList.push({ name, url, createAt });
+  localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
+  document.getElementById("name_input").value = "";
+  document.getElementById("url_input").value = "";
+  isAddBtnToggle();
+};
+
 let bookmarkAddBtn = false;
 bookmarkInputForm.style.display = "none";
 
-const isAddBtnClick = () => {
+const isAddBtnToggle = () => {
   bookmarkAddBtn = !bookmarkAddBtn;
   if (bookmarkAddBtn) {
     bookmarkInputForm.style.display = "flex";
@@ -14,4 +36,7 @@ const isAddBtnClick = () => {
 
 document
   .getElementById("bookmark_add_btn")
-  .addEventListener("click", isAddBtnClick);
+  .addEventListener("click", isAddBtnToggle);
+
+document.getElementById("add_btn").addEventListener("click", addBookmarkItem);
+document.getElementById("cancel_btn").addEventListener("click", isAddBtnToggle);
